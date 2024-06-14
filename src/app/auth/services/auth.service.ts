00f441d99846d4141dcf8b5c41463b83d5ff531c
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-14 12:22:42 +0200

    "371"

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -12,66 +12,76 @@ import { CheckTokenResponse } from '../interfaces/check-token.response';
 export class AuthService {
 
   private readonly baseUrl: string = environments.baseUrl;
-
-  private http = inject(HttpClient);
+  private http = inject( HttpClient );
 
   private _currentUser = signal<User|null>(null);
-  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking );
 
-  public currentUser = computed( ()=> this._currentUser());
-  public authStatus = computed( ()=> this._authStatus());
+  //! Al mundo exterior
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus = computed( () => this._authStatus() );
 
-  constructor() { }
 
-  login(email:string, password: string): Observable<boolean>{
-
-    const url = `${ this.baseUrl}/auth/login`;
-    const body = { email, password};
-
-    return this.http.post<LoginResponse>(url,body)
-      .pipe(
-        tap(({user, token})=>{
-          this._currentUser.set( user );
-          this._authStatus.set( AuthStatus.authenticated);
-          localStorage.setItem('token', token);
-        }),
-        map( ()=> true),
-
-        // Todo: errores
-        catchError( err => throwError (()=> err.error.message)
-      )
-      );
-
+  constructor() {
+    this.checkAuthStatus().subscribe();
   }
 
-  checkAuthStatus():Observable<boolean>{
+  private setAuthentication(user: User, token:string): boolean {
 
-    const url = `${ this.baseUrl}/auth/check-token`;
+    this._currentUser.set( user );
+    this._authStatus.set( AuthStatus.authenticated );
+    localStorage.setItem('token', token);
+
+    return true;
+  }
+
+
+
+
+  login( email: string, password: string ): Observable<boolean> {
+
+    const url  = `${ this.baseUrl }/auth/login`;
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>( url, body )
+      .pipe(
+        map( ({ user, token }) => this.setAuthentication( user, token )),
+        catchError( err => throwError( () => err.error.message ))
+      );
+  }
+
+  checkAuthStatus():Observable<boolean> {
+
+    const url   = `${ this.baseUrl }/auth/check-token`;
     const token = localStorage.getItem('token');
 
-    if ( !token)return of (false);
+    if ( !token ) {
+      this.logout();
+      return of(false);
+    }
 
     const headers = new HttpHeaders()
-    .set('Authorization',`Bearer ${ token }`);
+      .set('Authorization', `Bearer ${ token }`);
 
-    return this.http.get<CheckTokenResponse>(url, {headers})
-    .pipe(
-      map(({user, token})=>{
-        this._currentUser.set( user );
-        this._authStatus.set( AuthStatus.authenticated);
-        localStorage.setItem('token', token);
 
-      return true;
-    }),
+      return this.http.get<CheckTokenResponse>(url, { headers })
+        .pipe(
+          map( ({ user, token }) => this.setAuthentication( user, token )),
+          catchError(() => {
+            this._authStatus.set( AuthStatus.notAuthenticated );
+            return of(false);
+          })
+        );
 
-      catchError(( ) => {
-        this._authStatus.set(AuthStatus.notAuthenticated)
-        return of(false)}
-    )
-    );
+
   }
 
+  logout() {
+    localStorage.removeItem('token');
+    this._currentUser.set(null);
+    this._authStatus.set( AuthStatus.notAuthenticated );
 
+  }
 
 
 }
